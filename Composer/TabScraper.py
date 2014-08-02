5acbfc5bec0e1fcd94592c6e909e7eeb8c22507e
Janne Gröndahl <janne.grondahl@aalto.fi>
2014-08-02 23:33:14 +0300

    Work on tab scraper

--- Composer/TabScraper.py
+++ Composer/TabScraper.py
@@ -1,5 +1,6 @@
 import urllib
 import urllib2
+import re
 
 def main():
 	output_folder = "scraped_tabs/"
@@ -9,6 +10,16 @@ def main():
 	response = urllib2.urlopen(top100_url)
 	content = response.read()
 	
+	pattern = """a href="http://tabs.ultimate-guitar.com/.*/.*/.*.htm"""
+	print pattern
+	
+	tab_page_urls = re.findall(pattern, content)
+	for url in tab_page_urls:
+		print url
+		response = urllib2.urlopen(url)
+		content = response.read()		# read tab dl page
+		dl_link_pattern = """<input type='hidden' name='id' value='225441' id="tab_id">"""
+		
 	
 	ids = ["1494962", "1494961"]
 	for id in ids:
