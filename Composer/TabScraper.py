0a620bf49ecd57a1803a52068ca2c08e753047ff
Janne Gröndahl <janne.grondahl@aalto.fi>
2014-08-03 02:49:52 +0300

    Finish tab scraper (for now)

--- Composer/TabScraper.py
+++ Composer/TabScraper.py
@@ -3,27 +3,33 @@ import urllib2
 import re
 
 def main():
-	output_folder = "scraped_tabs/"
-	
-	top100_url = "http://www.ultimate-guitar.com/top/top100_pro.htm"
-	
-	response = urllib2.urlopen(top100_url)
-	content = response.read()
-	
-	pattern = """a href="http://tabs.ultimate-guitar.com/.*/.*/.*.htm"""
-	print pattern
-	
-	tab_page_urls = re.findall(pattern, content)
-	for url in tab_page_urls:
-		print url
-		response = urllib2.urlopen(url)
-		content = response.read()		# read tab dl page
-		dl_link_pattern = """<input type='hidden' name='id' value='225441' id="tab_id">"""
-		
-	
-	ids = ["1494962", "1494961"]
-	for id in ids:
-		urllib.urlretrieve("http://tabs.ultimate-guitar.com/tabs/download?id=" + id, output_folder + id + ".gp5")
-	
+  output_folder = "scraped_tabs/"
+  
+  top100_url = "http://www.ultimate-guitar.com/top/top100_pro.htm"
+  
+  response = urllib2.urlopen(top100_url)
+  content = response.read()
+  
+  id_name_pairs = []
+  pattern = """http://tabs.ultimate-guitar.com/.*/.*/.*.htm"""
+  tab_page_urls = re.findall(pattern, content)
+  for url in tab_page_urls:
+    print url
+    response = urllib2.urlopen(url)
+    content = response.read()         # read tab dl page
+    try:
+      dl_link_pattern = r"""<input type='hidden' name='id' value='(?P<song_id>.*)' id="tab_id">"""
+      id = re.search(dl_link_pattern, content).group('song_id')
+      name_grab_pattern = r"http://tabs.ultimate-guitar.com/.*/.*/(?P<song_name>.*)_guitar_pro.htm"
+      name = re.search(name_grab_pattern, url).group('song_name')
+      #print id, name
+      id_name_pairs.append((id, name))
+    except:
+      print "id / name grabbing failed, url:", url
+  
+  for id_name_pair in id_name_pairs:
+    id, name = id_name_pair
+    urllib.urlretrieve("http://tabs.ultimate-guitar.com/tabs/download?id=" + id, output_folder + name + ".gp5")
+  
 if __name__ == '__main__':
-	main()
+  main()
